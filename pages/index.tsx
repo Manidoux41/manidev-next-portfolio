bb6572b02543d72c17d41f7018eac7288ed6ee02
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-04-28 13:56:45 +0200

    test tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <title>Manidev's Portfolio</title>
       </Head>
 
-      <h1>Let's build an AWESOME portfolio</h1>
+      <h1 className="text-4xl font-semibold text-blue-500">Let's build an AWESOME portfolio</h1>
     </div>
   )
 }

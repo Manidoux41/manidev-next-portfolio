4bfb5ec3c7c1b83776cc86ee8d6b79e2a20714d3
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-04-28 14:25:44 +0200

    building Header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+import Header from "@/components/Header"
 import { NextPage } from "next"
 import Head from "next/head"
 
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       {/* Header */}
-
+      <Header />
       {/* Hero */}
 
       {/* About */}

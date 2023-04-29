614f6c7ff2eee501fc307fd7ada84e9e8a25fa2e
Manfred PARBATIA <superflyman90@gmail.com>
2023-04-29 23:51:23 +0200

    add link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,68 +1,70 @@
+
 import { SocialIcon } from "react-social-icons";
 import { motion } from 'framer-motion';
-import Link from "next/link";
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <motion.div
-            initial={{
-                x: -500,
-                opacity: 0,
-                scale:0.5
-            }}
-            animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1
-            }}
-            transition={{
-                duration: 1.5
-            }} 
-            className="flex flex-row items-center">
-            {/* Socials Icons */}
-            <SocialIcon 
-                url="https://www.youtube.com/channel/UCGyvuq2OKremAppYWxN76aA" 
-                fgColor="gray"
-                bgColor="transparent"
-            />
-            <SocialIcon 
-                url="https://github.com/Manidoux41" 
-                fgColor="gray"
-                bgColor="transparent"
-            />
-            <SocialIcon 
-                url="https://www.linkedin.com/in/manfred-p-804a72198/" 
-                fgColor="gray"
-                bgColor="transparent"
-            />
-        </motion.div>
-            <Link href='#hero'>Home</Link>
-        <motion.div 
-            initial={{
-                x: 500,
-                opacity: 0,
-                scale:0.5
-            }}
-            animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1
-            }}
-            transition={{
-                duration: 1.5
-            }}
-            className="flex flex-row items-center text-gray-300 cursor-pointer">
-            <SocialIcon 
-                className="cursor-pointer"
-                network="email"
-                fgColor="gray"
-                bgColor="transparent"
-            />
-            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
-        </motion.div>
+        
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale:0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className="flex flex-row items-center">
+                {/* Socials Icons */}
+                <SocialIcon
+                    url="https://www.youtube.com/channel/UCGyvuq2OKremAppYWxN76aA"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/Manidoux41"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://www.linkedin.com/in/manfred-p-804a72198/"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+            </motion.div>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale:0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className="flex flex-row items-center text-gray-300 cursor-pointer">
+                        <SocialIcon
+                            className="cursor-pointer"
+                            url="#contact"
+                            network="email"
+                            fgColor="gray"
+                            bgColor="transparent"
+                        />
+                        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
+            </motion.div>
+            
     </header>
   )
 }
